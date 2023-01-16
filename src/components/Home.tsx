2035ed701d497f28d6a1729e24ae96c591ec2083
salt-dev <salt-dev@salt-devs-MacBook-Air-9.local>
2023-01-16 12:28:26 +0100

    setup app

---
+++ src/components/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chips from './Chips';
+
+const Home = () => {
+  return (
+    <div className='text-lg'>
+      Home
+      // display tags with chips - all + 5 more 
+      <Chips />
+    // import components - each one of the tags
+    </div>
+  )
+}
+
+export default Home;
